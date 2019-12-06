bee92554e02540eadd504781a3ec4d400e57bfd3
Andrei Ivanov <dmkfasi@gmail.com>
2019-12-06 12:57:46 +0300

    Initial data added

--- conf/database.sql
+++ conf/database.sql
@@ -1,14 +1,19 @@
-CREATE TABLE 'task' (
+DROP TABLE IF EXISTS `task`;
+CREATE TABLE `task` (
 	`id`    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-	`username` TEXT,
-  `email` TEXT,
+	`userName` TEXT,
+  `userEmail` TEXT,
 	`description`  TEXT,
 	`isDone` BOOLEAN,
 	`isEdited` BOOLEAN
 );
 
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
 	`id`    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
 	`name`  TEXT,
 	`password` TEXT
 );
+
+INSERT INTO `user` (`name`, `password`) VALUES('admin', '123');
+
