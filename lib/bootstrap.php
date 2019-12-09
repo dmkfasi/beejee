b0e293183d824ed3871629ebd61a72dd899c746c
Andrei Ivanov <dmkfasi@gmail.com>
2019-12-09 22:53:29 +0300

    Rewired core classes

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -6,8 +6,10 @@ require_once 'conf/config.php';
 // Composer's autoloader for all the libraries in use
 require_once 'vendor/autoload.php';
 
+// Autoloader for app classes
 // Core Library objects
+include_once 'lib/registry.php';
 require_once 'lib/database.php';
 require_once 'lib/view.php';
+require_once 'app/Controller.php';
 require_once 'lib/router.php';
-
