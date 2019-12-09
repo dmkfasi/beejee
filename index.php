de0de7ce49608bbc010514cadbafec043a2a9dad
Andrei Ivanov <dmkfasi@gmail.com>
2019-12-09 22:51:27 +0300

    Quick debug function added

--- index.php
+++ index.php
@@ -3,9 +3,6 @@
 // Bootstrap everything in use
 require_once 'lib/bootstrap.php';
 
-//To get any table, use magic properties, they will be instantiated on demand:
-$tasks = $db->task;
-
-$view->setData($tasks);
-
-echo $view->__invoke();
+function debug($o) {
+    die(var_dump($o));
+}
