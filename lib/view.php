0bb3a78520cc223d5b84db1e05938cd13b4a22e2
Andrei Ivanov <dmkfasi@gmail.com>
2019-12-09 22:52:16 +0300

    Registry plunge added

--- lib/view.php
+++ lib/view.php
@@ -3,8 +3,6 @@
 use \Aura\View\ViewFactory;
 
 $view_factory = new ViewFactory();
-
-// Founding objects to use
 $view = $view_factory->newInstance();
 
 // Main layout for the website
@@ -13,10 +11,18 @@ $layout_registry->set('layout', 'views/layout.php');
 
 // View for the Task List
 $view_registry = $view->getViewRegistry();
+
+// Register view list
 $view_registry->set('task_list', 'views/task_list.php');
 $view_registry->set('task_add', 'views/task_add.php');
+
+// Register error list
+$view_registry->set('error_404', 'views/error_404.php');
+$view_registry->set('error_500', 'views/error_500.php');
 
 // Set default layout and view
 $view->setLayout('layout');
 $view->setView('task_list');
 
+// Adds ViewFactory object to the registry for further use
+registry::addService('view', $view);
