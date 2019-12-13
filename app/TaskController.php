66e4a3f6ae25f0a8cb5c502cdf11d957d04f4a60
Andrei Ivanov <dmkfasi@gmail.com>
2019-12-13 14:48:17 +0300

    Refactored for REST actions, save() method implemented

--- app/TaskController.php
+++ app/TaskController.php
@@ -1,34 +1,53 @@
 <?php
 
-use Zend\Diactoros\ServerRequest;
-use Aura\Router\Route;
-
 class TaskController extends Controller {
 
-    public function list(Route $r): string {
+    public function index(): string {
         // Gets task list for the page number in question
-        $tasks = $this->db->task->select()
-            ->page($r->attributes['id'])
-            ->perPage(3);
+        $query = $this->db->task->select()
+        ->page(1)
+        ->perPage(3)
+        ->orderBy('id DESC');
+
+        $tasks = $query->get();
+        $pagination = $query->getPageInfo();
         
-        // Fills and displays task list
-        $this->view->setView('task_list', $tasks);
-        
+        // Fills task list and pagination data
+        $this->view->addData([
+            'tasks' => $tasks,
+            'pagination' => $pagination,
+        ]);
+
+        $this->view->setView('task_list');
         return $this->view->__invoke();
     }
-    
-    public function add(): string {
+
+    public function create(): string {
         // Shows new task form
         $this->view->setView('task_add');
         
         return $this->view->__invoke();
     }
     
-    public function save(Route $r): string {
-        // Fill and display task list
-        $this->view->setView('task_add');
-        
-        return $this->view->__invoke();
+    public function store(): void {
+        // TODO Extra validation and sanitization
+        $values = input()->all([
+            'username',
+            'email',
+            'description'
+        ]);
+
+        // Get new active record object
+        $task = $this->db->task->create();
+        // Collect inputs for the object
+        $task->username     = htmlspecialchars($values['username']);
+        $task->email        = htmlspecialchars($values['email']);
+        $task->description  = htmlspecialchars($values['description']);
+
+        // Persist object
+        $task->save();
+
+        redirect(url('task'));
     }
-    
+
 }
