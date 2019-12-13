743752972bf978741f4fe037e4cae70d47193754
Andrei Ivanov <dmkfasi@gmail.com>
2019-12-13 14:49:41 +0300

    Autoloader added

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -7,9 +7,15 @@ require_once 'conf/config.php';
 require_once 'vendor/autoload.php';
 
 // Autoloader for app classes
+function loadController($class) {
+    $path = 'app/';
+    require_once "app/{$class}.php";
+}
+
+spl_autoload_register('loadController');
+
 // Core Library objects
 include_once 'lib/registry.php';
 require_once 'lib/database.php';
 require_once 'lib/view.php';
-require_once 'app/Controller.php';
 require_once 'lib/router.php';
