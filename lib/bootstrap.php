fe61f14d72ab9397de235b656ec41cb882ab4e4c
Andrei Ivanov <dmkfasi@gmail.com>
2019-12-15 00:20:06 +0300

    Rewired coupled Core Objects, debug() moved here, too

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -7,15 +7,22 @@ require_once 'conf/config.php';
 require_once 'vendor/autoload.php';
 
 // Autoloader for app classes
+spl_autoload_register('loadController');
+
+// Core Library objects
+// TODO Decouple everything, use a container and stuff
+require_once 'lib/registry.php';
+require_once 'lib/database.php';
+require_once 'lib/AuraCustomAdapter.php';
+require_once 'lib/auth.php';
+require_once 'lib/view.php';
+require_once 'lib/router.php';
+
 function loadController($class) {
     $path = 'app/';
     require_once "app/{$class}.php";
 }
 
-spl_autoload_register('loadController');
-
-// Core Library objects
-include_once 'lib/registry.php';
-require_once 'lib/database.php';
-require_once 'lib/view.php';
-require_once 'lib/router.php';
+function debug($o) {
+    die(var_dump($o));
+}
