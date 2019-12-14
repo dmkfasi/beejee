173765c5ae0388eb9a758d84d76b35a5b8fb2f9d
Andrei Ivanov <dmkfasi@gmail.com>
2019-12-15 00:15:41 +0300

    Login/Logout toggle added

--- views/layout.php
+++ views/layout.php
@@ -7,8 +7,15 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <!-- Bootstrap core JavaScript-->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
-		<title>BeeJee Task List Website</title>
+    <!--  DataTables -->
+	<link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+	<script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+	<script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
+
+	<title>BeeJee Task List Website</title>
   </head>
   <body>
 		<div class="container pt-4">
@@ -17,8 +24,16 @@
 					<a href="<?=url('task.create')?>" class="btn btn-primary">Add a Task</a>
 					<a href="<?=url('task.index')?>" class="btn btn-primary">Task List</a>
 				</div>
+				<div class="col text-center my-auto">
+					<?=Controller::getSessionMessage('user_login_message', false)?>
+				</div>
 				<div class="col text-right">
-					<a href="/user/login" class="btn btn-primary">Login</a>
+					<?php if (\Aura\Auth\Status::VALID === $this->user_auth()->getStatus()) {
+					   printf('<a href="%s" class="btn btn-primary">%s</a>', url('user.logout'), 'Logout');
+					} else {
+					   printf('<a href="%s" class="btn btn-primary">%s</a>', url('user.login'), 'Login');
+					}
+					?>
 				</div>
 			</div>
 		</div>
