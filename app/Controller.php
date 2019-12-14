60ea2411cd0085d19083ce333bd214bd8506d03c
Andrei Ivanov <dmkfasi@gmail.com>
2019-12-15 00:12:20 +0300

    Session Message exchange static functions added

--- app/Controller.php
+++ app/Controller.php
@@ -1,15 +1,43 @@
 <?php
 
-use \SimpleCrud\Database;
+class Controller
+{
 
-class Controller {
-
-    protected $db = null;
+    protected $db   = null;
     protected $view = null;
+    protected $auth = null;
 
-    public function __construct() {
-        $this->db = registry::getService('db');
+    public function __construct()
+    {
+        $this->db   = registry::getService('db');
         $this->view = registry::getService('view');
+        $this->auth = registry::getService('auth');
     }
 
+    // TODO Move this to some Utils class
+    public static function getSessionMessage(string $section, bool $removeIt = true): ?string
+    {
+        if (isset($_SESSION[$section])) {
+            $message = $_SESSION[$section];
+
+            // Clean this up upon request
+            if ($removeIt) {
+                self::clearSessionMessage($section);
+            }
+
+            return $message;
+        } else {
+            return false;
+        }
+    }
+
+    public static function setSessionMessage(string $section, string $message): void
+    {
+        $_SESSION[$section] = $message;
+    }
+
+    public static function clearSessionMessage(string $section): void
+    {
+        unset($_SESSION[$section]);
+    }
 }
