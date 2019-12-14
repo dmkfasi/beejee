5034c1359fd6af71f5b5c3d977aef30537776bc6
Andrei Ivanov <dmkfasi@gmail.com>
2019-12-15 00:18:31 +0300

    UserController helper added

--- lib/view.php
+++ lib/view.php
@@ -5,24 +5,30 @@ use \Aura\View\ViewFactory;
 $view_factory = new ViewFactory();
 $view = $view_factory->newInstance();
 
+$helpers = $view->getHelpers();
+$helpers->set('user_auth', function(){
+    return new UserController;
+});
+
 // Main layout for the website
 $layout_registry = $view->getLayoutRegistry();
 $layout_registry->set('layout', 'views/layout.php');
 
-// View for the Task List
+// Register view list
 $view_registry = $view->getViewRegistry();
 
-// Register view list
 $view_registry->set('task_list', 'views/task_list.php');
 $view_registry->set('task_add', 'views/task_add.php');
 
-// Register error list
+$view_registry->set('user_auth', 'UserController@getStatus');
+$view_registry->set('user_login', 'views/user_login.php');
+$view_registry->set('user_logout', 'views/user_logout.php');
+
 $view_registry->set('error_404', 'views/error_404.php');
 $view_registry->set('error_500', 'views/error_500.php');
 
 // Set default layout and view
 $view->setLayout('layout');
-$view->setView('task_list');
 
-// Adds ViewFactory object to the registry for further use
+// Add ViewFactory object to the registry for further use
 registry::addService('view', $view);
