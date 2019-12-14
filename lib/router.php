2d90a1aa298b0758adf5a89faf9be1e561e33264
Andrei Ivanov <dmkfasi@gmail.com>
2019-12-15 00:18:45 +0300

    UserController routes added

--- lib/router.php
+++ lib/router.php
@@ -7,10 +7,12 @@ use Pecee\SimpleRouter\SimpleRouter;
 
 // Default route as per description
 SimpleRouter::get('/', 'TaskController@index');
-
 // Resource controller based route
 SimpleRouter::resource('task', TaskController::class);
 
-SimpleRouter::controller('task/page/{id}', 'TaskController@index');
+// User Authentication route
+SimpleRouter::get('/user/login',    'UserController@login')->name('user.login');
+SimpleRouter::get('/user/logout',   'UserController@logout')->name('user.logout');
+SimpleRouter::post('/user/auth',    'UserController@auth')->name('user.auth');
 
 SimpleRouter::start();
