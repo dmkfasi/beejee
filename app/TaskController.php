eafa86a9c7809df8c7a63e9f60aab59adfe12dcd
Andrei Ivanov <dmkfasi@gmail.com>
2019-12-15 17:56:19 +0300

    Extra checks for data in store() method, setDone() method added for HXR

--- app/TaskController.php
+++ app/TaskController.php
@@ -28,32 +28,58 @@ class TaskController extends Controller
     public function store(): void
     {
         // TODO Extra validation and sanitization
-        $values = input()->all([
+        list(
+            $id,
+            $username,
+            $email,
+            $description,
+            $isDone
+        ) = input()->all([
+            'id',
             'username',
             'email',
-            'description'
+            'description',
+            'isDone',
         ]);
 
-        try {
-            // Get new active record object
-            $task = $this->db->task->create();
-            // Collect inputs for the object
-            $task->username     = htmlspecialchars($values['username']);
-            $task->email        = htmlspecialchars($values['email']);
-            $task->description  = htmlspecialchars($values['description']);
+        // Save only tasks with proper data handed over
+        if (isset($username) && isset($email)) {
+            try {
+                // Get new active record object
+                $task = $this->db->task->create();
+                // Collect inputs for the object
+                $task->username     = htmlspecialchars($username);
+                $task->email        = htmlspecialchars($email);
+                $task->description  = htmlspecialchars($description);
 
-            // Persist object
-            $task->save();
+                // Persist object
+                $task->save();
 
-            // Send a message with HTTP redirect
-            self::setSessionMessage('task_save_message', 'Task saved successfully');
-        } catch (\Exception $e) {
-            // TODO Handle the error saving
-            self::setSessionMessage('task_save_message', "Unable to save new Task: {$e->getMessage()}");
+                // Send a message with HTTP redirect
+                self::setSessionMessage('task_save_message', 'Task saved successfully');
+            } catch (\Exception $e) {
+                // TODO Handle the error saving
+                self::setSessionMessage('task_save_message', "Unable to save new Task: {$e->getMessage()}");
+            }
         }
 
         // Redirect with a message, for a reason unknown, it does not work this way
         redirect(url('task', [ 'message' => 'Data saved successfully' ]), 301);
     }
 
+    public function setDone() {
+        // Do not allow unauthenticated user update Task's status
+        if ($this->auth->isValid()) {
+            // Find the Task in question
+            $id = input()->find('id')->getValue();
+            $task = $this->db->task[$id];
+
+            // Update its status
+            $task->isDone = 1;
+            // TODO Handle errors
+            $task->save();
+        } else {
+            throw new \Exception('Unauthenticated');
+        }
+    }
 }
