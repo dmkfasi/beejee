75d0abea3329ab2b4f1223a1d911205798c36b80
Andrei Ivanov <dmkfasi@gmail.com>
2019-12-15 19:11:24 +0300

    Fixed for MySQL syntax

--- conf/database.sql
+++ conf/database.sql
@@ -1,19 +1,20 @@
 DROP TABLE IF EXISTS `task`;
 CREATE TABLE `task` (
-	`id`    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-	`userName` TEXT,
-  `userEmail` TEXT,
+	`id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`username` TEXT,
+  `email` TEXT,
 	`description`  TEXT,
 	`isDone` BOOLEAN,
-	`isEdited` BOOLEAN
+	`isEdited` BOOLEAN,
+	UNIQUE KEY (`id`)
 );
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-	`id`    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`id`    INTEGER NOT NULL AUTO_INCREMENT,
 	`name`  TEXT,
-	`password` TEXT
+	`password` TEXT,
+	UNIQUE KEY (`id`)
 );
 
 INSERT INTO `user` (`name`, `password`) VALUES('admin', '123');
-
