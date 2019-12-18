8ebe9cbb7974484890795edca5a7383ac1c9ef33
Andrei Ivanov <dmkfasi@gmail.com>
2019-12-18 10:39:28 +0300

    Revert "Fixed for MySQL syntax"
    This reverts commit 75d0abea3329ab2b4f1223a1d911205798c36b80.

--- conf/database.sql
+++ conf/database.sql
@@ -1,20 +1,19 @@
 DROP TABLE IF EXISTS `task`;
 CREATE TABLE `task` (
-	`id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-	`username` TEXT,
-  `email` TEXT,
+	`id`    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`userName` TEXT,
+  `userEmail` TEXT,
 	`description`  TEXT,
 	`isDone` BOOLEAN,
-	`isEdited` BOOLEAN,
-	UNIQUE KEY (`id`)
+	`isEdited` BOOLEAN
 );
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-	`id`    INTEGER NOT NULL AUTO_INCREMENT,
+	`id`    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
 	`name`  TEXT,
-	`password` TEXT,
-	UNIQUE KEY (`id`)
+	`password` TEXT
 );
 
 INSERT INTO `user` (`name`, `password`) VALUES('admin', '123');
+
